7953fff007448c8ef8ed5885db80fe5e64cb1dd8
Ishtiak Al Humaidi <ishtiakalhumaidi@gmail.com>
2025-07-27 00:29:28 +0600

    Update 1-A. Odd or Even.py

--- 1-A. Odd or Even.py
+++ 1-A. Odd or Even.py
@@ -1,19 +1,9 @@
 import sys
+
 count = int(sys.stdin.readline())
 for _ in range(count):
-    a = sys.stdin.readline().strip()
-    arithmetic = a.split(' ')
-    first = int(arithmetic[1])
-    exp = arithmetic[2]
-    second = int(arithmetic[3])
-    if exp=="+":
-        result = first + second
-    elif exp == "-":
-        result = first - second
-    elif exp == '*':
-        result = first * second
-    elif exp=='/':
-        result =first / second
+    n = int(sys.stdin.readline())
+    if n%2==0:
+        sys.stdout.write(f"{n} is an Even number.\n")
     else:
-        result = 'expression not valid'
-    sys.stdout.write(str(result) + '\n')
+        sys.stdout.write(f"{n} is an Odd number.\n")
