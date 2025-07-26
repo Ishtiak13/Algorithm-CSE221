f6dedec801f51bf0ae2220931dc7a04ddebabcfa
Ishtiak Al Humaidi <ishtiakalhumaidi@gmail.com>
2025-07-27 00:27:44 +0600

    Update 1-D. Is Sorted.py

--- 1-D. Is Sorted.py
+++ 1-D. Is Sorted.py
@@ -1,15 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-arr = [int(i) for i in input().split()]
-
-c = True
-while c:
-    c = False
-    for i in range(n - 1):
-        if arr[i] % 2 == arr[i + 1] % 2 and arr[i] > arr[i + 1]:
-            arr[i], arr[i + 1] = arr[i + 1], arr[i]
-            c = True
-
-sys.stdout.write(' '.join(map(str, arr)) + '\n')
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = [int(x) for x in input().split()]
+    
+    is_sorted = True
+    for i in range(1, n):
+        if arr[i] < arr[i - 1]:
+            is_sorted = False
+            break
+    
+    sys.stdout.write("YES\n" if is_sorted else "NO\n")
