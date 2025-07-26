46dc9ec4e86707b78716894d9bd1d6c358f1536e
Ishtiak Al Humaidi <ishtiakalhumaidi@gmail.com>
2025-07-27 00:28:53 +0600

    Update 1-B. Can you solve Arithmetic Expressions.py

--- 1-B. Can you solve Arithmetic Expressions.py
+++ 1-B. Can you solve Arithmetic Expressions.py
@@ -1,8 +1,19 @@
 import sys
- 
-T = int(sys.stdin.readline())
- 
-for _ in range(T):
-    N = int(sys.stdin.readline())
-    total = N * (N + 1) // 2
-    sys.stdout.write(str(total) + '\n')
+count = int(sys.stdin.readline())
+for _ in range(count):
+    a = sys.stdin.readline().strip()
+    arithmetic = a.split(' ')
+    first = int(arithmetic[1])
+    exp = arithmetic[2]
+    second = int(arithmetic[3])
+    if exp=="+":
+        result = first + second
+    elif exp == "-":
+        result = first - second
+    elif exp == '*':
+        result = first * second
+    elif exp=='/':
+        result =first / second
+    else:
+        result = 'expression not valid'
+    sys.stdout.write(str(result) + '\n')
