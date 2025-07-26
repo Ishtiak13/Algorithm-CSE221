5935fc6a1e4ba05ab0a6b7ed122ad3dcedc20516
Ishtiak Al Humaidi <ishtiakalhumaidi@gmail.com>
2025-07-27 00:28:22 +0600

    Update 1-C. Fast Sum.py

--- 1-C. Fast Sum.py
+++ 1-C. Fast Sum.py
@@ -1,15 +1,8 @@
 import sys
-input = sys.stdin.readline
 
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    arr = [int(x) for x in input().split()]
-    
-    is_sorted = True
-    for i in range(1, n):
-        if arr[i] < arr[i - 1]:
-            is_sorted = False
-            break
-    
-    sys.stdout.write("YES\n" if is_sorted else "NO\n")
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    N = int(sys.stdin.readline())
+    total = N * (N + 1) // 2
+    sys.stdout.write(str(total) + '\n')
